8bfcacce22e6fb9891f2e507d950b119baaf06da
SHUVO-official <the.shuvo.dev@gmail.com>
2025-07-27 00:15:54 +0600

    Add percentage of 5 numbers

---
+++ Basics/Percentage-5-numbers.cpp
@@ -0,0 +1,10 @@
+/* This code calculates the percentage of five numbers. */
+#include <iostream>
+using namespace std;
+    int main()
+    {
+        float x1=2,x2=4,x3=4,x4=5,x5=9,percentage;
+        percentage=(x1+x2+x3+x4+x5)/5;
+        cout<<"The pecentage is:"<<percentage <<"%";
+
+    }
