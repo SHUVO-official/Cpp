b2d42def70fcb89a7c8ad4b981ee1a4c62941166
SHUVO-official <the.shuvo.dev@gmail.com>
2025-07-27 00:21:00 +0600

    Demonstration Veriables

---
+++ Basics/veriables.cpp
@@ -0,0 +1,11 @@
+/* This code demonstrates the use of variables in C++. */
+#include<iostream>
+using namespace std;
+    
+int main() {
+    int x;
+    x=19;
+    cout<<x<<endl;
+    x=x+200;
+    cout<<"the number is:"<<x;
+}
