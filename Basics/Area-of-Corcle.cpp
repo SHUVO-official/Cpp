ee789d47224ea6c0884192f5a8e388b681e8a24a
SHUVO-official <the.shuvo.dev@gmail.com>
2025-07-26 23:55:51 +0600

    Modify area of circle

---
+++ Basics/Area-of-Corcle.cpp
@@ -0,0 +1,9 @@
+/* This code calculates the circumference of a circle. */
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        float r=10,pi=3.1415,circumference;
+        circumference=2*pi*r;
+        cout<<"The circumference o the circle is:"<<circumference;
+    }
