a4068b7e12adf6c951d4453a9b0e2fa2d3c5940c
SHUVO-official <the.shuvo.dev@gmail.com>
2025-07-28 23:15:42 +0600

    Add weekday in switch

---
+++ Conditionals/Switch/Weekday.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+    int main()
+        {
+            int n;
+            cin>>n;
+            switch(n)
+                {
+                    case 1:
+                    cout<<"Saterday";
+                    break;
+                    case 2:
+                    cout<<"Sunday";
+                    break;
+                    case 3:
+                    cout<<"Monday";
+                    break;
+                    case 4:
+                    cout<<"Tuesday";
+                    break;
+                    case 5:
+                    cout<<"Wednesday";
+                    break;
+                    case 6:
+                    cout<<"Thusday";
+                    break;
+                    case 7:
+                    cout<<"Friday";
+                    break;
+                    default:
+                    cout<<"Invalid";
+                    
+                }
+        }
