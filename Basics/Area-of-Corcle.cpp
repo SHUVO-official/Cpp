2d3f40ae64320b574065abb1ab37ad36f64decfa
SHUVO-official <the.shuvo.dev@gmail.com>
2025-07-31 10:37:30 +0600

    Modify AREA

--- Basics/Area-of-Corcle.cpp
+++ Basics/Area-of-Corcle.cpp
@@ -3,6 +3,7 @@
 using namespace std;
     int main()
     {
+        //I
         float r=10,pi=3.1415,circumference;
         circumference=2*pi*r;
         cout<<"The circumference of circle is:"<<circumference;
