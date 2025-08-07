d95e81ecf3b52652c145c938ce5413991f49ded1
SHUVO-official <the.shuvo.dev@gmail.com>
2025-08-08 00:20:19 +0600

    Modify Area of circle

--- Basics/Area-of-Circle.cpp
+++ Basics/Area-of-Circle.cpp
@@ -1,10 +1,9 @@
-/* This code calculates the circumference of a circle. */
-#include<iostream>
+/*This code demonstrate the area of circle*/
+#include <iostream>
 using namespace std;
     int main()
-    {
-        
-        float r=10,pi=3.1415,circumference;
-        circumference=2*pi*r;
-        cout<<"The circumference of circle is:"<<circumference;
-    }
+        {
+            double r=10,pi=3.1416,area;
+            area=pi*(r*r);
+            cout<<"The area of the circle is : "<<area;
+        }
