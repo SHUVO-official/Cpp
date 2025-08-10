0f87c50cb7f5a0520240d28de896d1236f794ec1
SHUVO-official <the.shuvo.dev@gmail.com>
2025-08-10 15:46:57 +0600

    Add New item

---
+++ Patern-Printing/pp-2/Num-spiral-3.cpp
@@ -0,0 +1,31 @@
+// 4 4 4 4 4 4 4 
+// 4 3 3 3 3 3 4 
+// 4 3 2 2 2 3 4 
+// 4 3 2 1 2 3 4 
+// 4 3 2 2 2 3 4 
+// 4 3 3 3 3 3 4 
+// 4 4 4 4 4 4 4
+
+#include <iostream>
+using namespace std;
+    int main()
+        {
+            int n;
+            cin>>n;
+            //এই ধাঁচটা আগে বানাতে হবে।
+            // 1 1 1 1 
+            // 1 2 2 2 
+            // 1 2 3 3 
+            // 1 2 3 4 
+            for(int i=1;i<=2*n-1;i++)
+                {
+                    for(int j=1;j<=2*n-1;j++)
+                    {
+                        int a=i,b=j;
+                        if(a>n) a=2*n-i;
+                        if(b>n) b=2*n-j;
+                        cout<<n-min(a,b)+1<<" ";
+                    }
+                    cout<<endl;
+                }
+        }
