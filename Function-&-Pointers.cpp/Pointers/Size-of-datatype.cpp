7a5ba42fd065c72667f353e1bf3e24d41d76710c
SHUVO-official <the.shuvo.dev@gmail.com>
2025-08-20 20:31:00 +0600

    Add new

---
+++ Function-&-Pointers.cpp/Pointers/Size-of-datatype.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+    int main()
+        {
+            cout<<sizeof(bool)<<" Byte"<<endl;
+            cout<<sizeof(int)<<" Byte"<<endl;
+            cout<<sizeof(float)<<" Byte"<<endl;
+            cout<<sizeof(double)<<" Byte"<<endl;
+            cout<<sizeof(long long)<<" Byte"<<endl;
+        }
