e96e7be1ddd3c5206cfd3c6ca273aa0effc7b386
SHUVO-official <the.shuvo.dev@gmail.com>
2025-11-08 20:16:44 +0600

    Modify file

--- Loops/1toN-even-sum.cpp
+++ Loops/1toN-even-sum.cpp
@@ -5,27 +5,27 @@ using namespace std;
     {
                         /*For loop*/
 
-        //    int n,i=0,sum=0;
-        //    cout<<"Enter a number: ";
-        //    cin>>n;
-        //    for( ;i<=n;i+=2) 
-        //    {
-        //     sum+=i;
-        //    }    
-        //    cout<<sum;   
+           int n,i=0,sum=0;
+           cout<<"Enter a number: ";
+           cin>>n;
+           for( ;i<=n;i+=2) 
+           {
+            sum+=i;
+           }    
+           cout<<sum;   
              
-           
+        }  
 
                         /*While loop*/
 
 
-                int i=0,n,sum=0;
-                cout<<"Enter a number: ";
-                cin>>n;
-                while(i<=n)
-                        {
-                            sum+=i;
-                            i+=2;
-                        }
-                        cout<<sum;
-    }
+    //             int i=0,n,sum=0;
+    //             cout<<"Enter a number: ";
+    //             cin>>n;
+    //             while(i<=n)
+    //                     {
+    //                         sum+=i;
+    //                         i+=2;
+    //                     }
+    //                     cout<<sum;
+    // }
