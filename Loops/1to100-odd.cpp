f42d140faceeb07046f2442ce2b1d3663c125a0a
SHUVO-official <the.shuvo.dev@gmail.com>
2025-11-12 20:12:18 +0600

    Modify item

--- Loops/1to100-odd.cpp
+++ Loops/1to100-odd.cpp
@@ -2,8 +2,10 @@
 using namespace std;
     int main()
     {  
+
         for(int i=1;i<=100;i++)
         {
             if(i%2!=0) cout<<i<<endl;
-        }        
+        }
+                
     }
