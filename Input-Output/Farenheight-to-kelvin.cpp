8431950a1f7879f821f618488726c876eead0275
SHUVO-official <the.shuvo.dev@gmail.com>
2025-11-13 22:23:01 +0600

    Modify item

--- Input-Output/Farenheight-to-kelvin.cpp
+++ Input-Output/Farenheight-to-kelvin.cpp
@@ -8,7 +8,7 @@ int main() {
     cout << "Enter temperature in Fahrenheit: ";
     cin >> fahrenheit;
     
-            // সঠিক ফর্মুলা: (F - 32) × 5/9 + 273.15
+    // সঠিক ফর্মুলা: (F - 32) × 5/9 + 273.15        
     kelvin = (fahrenheit - 32) * 5.0/9.0 + 273.15;
     
     cout << "Temperature in Kelvin: " << kelvin;
