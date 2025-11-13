6131b2b23741ed68bce303b12355293d6d09468f
SHUVO-official <the.shuvo.dev@gmail.com>
2025-11-13 22:18:35 +0600

    Modify item

--- Input-Output/centigrade-to-farenhight.cpp
+++ Input-Output/centigrade-to-farenhight.cpp
@@ -3,11 +3,9 @@
 using namespace std;
     int main()
     {
-
         float C,F;
         cout<<"Enter the value of celcius: ";
         cin>>C;
         F=(9*C)/5+32;
         cout<<"The value of Farenheight is: "<<F;
-        
-    }
+     }
