ea7deea61586fd132e64cf27606351bea9b9b331
SHUVO-official <the.shuvo.dev@gmail.com>
2026-04-11 13:19:28 +0600

    Add new item

---
+++ 2D Array/2D Array-01/Basics/Vector/Declaration.cpp
@@ -0,0 +1,17 @@
+  #include <iostream>
+  using namespace std;
+    int main()
+        {
+            // int a[4][2]={{2,3},{5,7},{5,1},{9,2}};
+            // int a[4][2]={2,3,5,7,5,1,9,2};
+            int a[][2]={2,3,5,7,5,1,9,2}; //eita darao ager 2 tar motoi bujhay mane row always lekhar dorkar nai
+
+            // int a[2][0]={2,3,5,7,5,1,9,2}; //eita bhul karon column always ditei hobe
+
+            //row=4 --> index=0 1 2 3
+            //column=2 -->index=0 1
+
+          
+
+            
+        }
