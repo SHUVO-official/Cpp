f77b8764518df6fc9d72363ff0c458274ed15b12
SHUVO-official <the.shuvo.dev@gmail.com>
2026-05-17 22:13:41 +0600

    modify item

--- 4. Loops/1to100-odd.cpp
+++ 4. Loops/1to100-odd.cpp
@@ -2,7 +2,9 @@
 using namespace std;
     int main()
     {  
-        for(int i=1;i<=100;i++)
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++)
         {
             if(i%2!=0) cout<<i<<endl;
         }              
