d1567017f60644902f175d688a3a0f51e782f85c
SHUVO-official <the.shuvo.dev@gmail.com>
2026-06-09 22:00:49 +0600

    Add new item

--- 7. Array/Array-2/VECTOR/Vector-QUESTION/Reverse-part-of-array.cpp
+++ 7. Array/Array-2/VECTOR/Vector-QUESTION/Reverse-part-of-array.cpp
@@ -28,7 +28,6 @@ using namespace std;
                     v[i]=v[j];
                     v[j]=temp;
                 }
-                return;
                     }    
     int main()
         {
