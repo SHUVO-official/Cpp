c88751cae51bdd153964a6164cea8d007006f5fb
SHUVO-official <the.shuvo.dev@gmail.com>
2026-07-07 18:19:54 +0600

    Add new item

--- 18.Binary Tree/Binary Tree 2/Question/level-wise-display-using-queue.cpp
+++ 18.Binary Tree/Binary Tree 2/Question/level-wise-display-using-queue.cpp
@@ -13,31 +13,7 @@ using namespace std;
                     this->right=NULL;
                 }
         };
-    int level(Node* root)
-        {
-            if(root == NULL) return 0;
-            return 1+max(level(root->left),level(root->right));
-        }    
-    void nthlevel(Node* root,int curr,int level)
-        {
-            if(root == NULL) return;  //base
-            if(curr==level)
-                 {
-                    cout<<root->val<<" ";
-                    return;
-                }  //root
-            nthlevel(root->left,curr+1,level);  //left
-            nthlevel(root->right,curr+1,level); //right
-        }
-    void levelorder(Node* root)
-        {
-            int n=level(root);
-            for(int i=1;i<=n;i++)
-                {
-                    nthlevel(root,1,i);
-                    cout<<endl;
-                }
-        }    
+       
     //      using Queue
     void levelOrderQueue(Node* root)
         {
